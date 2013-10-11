9e7f89ac9b28c4b8c02556dde2824f5f13c9977b
AliaR <student@c02kw1jnfh51.dalton.org>
2013-10-11 12:27:52 -0400

    added depth method, added pop from a certain point method

--- IntStack.java
+++ IntStack.java
@@ -2,10 +2,9 @@ public class IntStack {
     int[] stack;
     int top;
 
-    public Int Stack(int size) {
+    public IntStack(int size) {
 	top=0;
 	stack = new int[size];
-
     }
 
     boolean isEmpty() {
@@ -17,20 +16,46 @@ public class IntStack {
 	stack[top++]=num;       
     }
 
-    int pop() {
-	//--happens before and changes top
-	return stack[--top]
+    //Alia
+    int depth(){
+	//returns the depth, same as top
+	int depth = top;
+	return depth;
     }
+
+    //Alia
+    int[] popfrompoint(int popoint){
+	//pop all until top is at a certain pointt
+	// make int array of things popped that will be returned
+	// make int for position in popped that the thing that is popped out will be placed in
+	int i = 0;
+	int[] popped = new int[top- popoint];
+	    // if top is greater than popoint, place the point that is popped out of the stack into a new array, called popped 
+	while (top > popoint){
+	    popped[i++] = stack[top--];
+	}
+	// print the array
+	for (int b  = 0; b < popped.length; b++){
+	    System.out.print(popped[b] + " ");
+}
+	return popped;	
+    }
+
+      int pop() {
+	//--happens before and changes top
+   	return stack[--top];
+     }
+
     int peek() {
-	return stack[top-1}
+	return stack[top-1];
     }
 
     public static void main(String[] args) {
-	IntStack is = new Int Stack(10);
+	IntStack is = new IntStack(10);
 	is.push(4);
 	is.push(5);
 	is.push(6);
 	int k = is.pop();
 	System.out.println(k);
-
+    }
 }
