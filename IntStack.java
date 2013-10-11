5448517451a8d35fa75a5db12db73d9a6b310277
michael as <michael_aguero-sinclair@dalton.org>
2013-10-11 11:56:36 -0400

    added peekAll and peekAt

--- IntStack.java
+++ IntStack.java
@@ -1,8 +1,10 @@
+import java.util.*;
+
 public class IntStack {
     int[] stack;
     int top;
 
-    public Int Stack(int size) {
+    public IntStack(int size) {
 	top=0;
 	stack = new int[size];
 
@@ -19,18 +21,38 @@ public class IntStack {
 
     int pop() {
 	//--happens before and changes top
-	return stack[--top]
+	return stack[--top];
     }
     int peek() {
-	return stack[top-1}
+	return stack[top-1];
     }
 
+    int[] peekAll() {
+	int[] peekAll = new int[stack.length];
+	for(int i=0; i<top; i++) {
+	    peekAll[i] = stack[i];
+        }
+	return peekAll;
+    }
+    
+    int peekAt(int position) {
+	if(position>=top) return 0;
+	return stack[position];
+
+    }
+
+
     public static void main(String[] args) {
-	IntStack is = new Int Stack(10);
+	IntStack is = new IntStack(10);
 	is.push(4);
 	is.push(5);
 	is.push(6);
 	int k = is.pop();
 	System.out.println(k);
+	int[] m = is.peekAll();
+	int p = is.peekAt(2);
+	System.out.println(Arrays.toString(m));
+	System.out.println(p);
+    }
 
 }
