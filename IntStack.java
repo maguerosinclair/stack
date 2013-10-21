f83df0f834b15bcb1c8aeac73af7239e48d7a573
Rachel Kasdin <Rachel_Kasdin@dalton.org>
2013-10-21 13:30:48 -0400

    reverse stack and sort

--- IntStack.java
+++ IntStack.java
@@ -2,10 +2,16 @@ public class IntStack {
     int[] stack;
     int top;
 
-    public Int Stack(int size) {
+    public IntStack(int size) {
 	top=0;
 	stack = new int[size];
 
+    }
+
+    public void print() {
+	for(int i=0; i<top; i++){
+	    System.out.println(stack[(top-1)-i]);
+	}    
     }
 
     boolean isEmpty() {
@@ -17,20 +23,61 @@ public class IntStack {
 	stack[top++]=num;       
     }
 
-    int pop() {
-	//--happens before and changes top
-	return stack[--top]
+    int pop(){
+	//--happens before and changes top only if there is something left to pop
+	if(isEmpty()==false){
+	   return stack[--top]; 
+	}
+	return -1; 
     }
     int peek() {
-	return stack[top-1}
+	return stack[top-1];
+    }
+
+
+    IntStack reverseStack(){
+	//make a newStack thats empty
+	IntStack newStack = new IntStack(stack.length);	
+	while (! isEmpty()){  //repeat until isEmpty()
+	    int p = pop();  //pop the top item and push that onto the newStack
+ 	    newStack.push(p); 
+        }  
+	return newStack; //return newStack
+    }
+
+    IntStack  sort() {
+	//create two new empty stacks
+      	IntStack newStack1 = new IntStack(stack.length);
+	IntStack newStack2 = new IntStack(stack.length);
+	while (! isEmpty()){ //repeat until isEmpty()
+	    int p = peek(); 	//peek top number of original stack
+	    if(p<5){ //if its less than five, pop it and push it onto the first new stack
+		newStack1.push(p);
+	    }
+	    else {  //if its greater than or equal to five, pop it and push it onto the second new stack
+		newStack2.push(p); 
+	    }
+	}
+        newStack1.push(newStack2.pop()); //pop the top number of the second new stack and push it onto the first new stack
+	return newStack1; 
     }
 
     public static void main(String[] args) {
-	IntStack is = new Int Stack(10);
+	IntStack is = new IntStack(10);
 	is.push(4);
 	is.push(5);
 	is.push(6);
 	int k = is.pop();
 	System.out.println(k);
 
+	//reverse stack
+	IntStack is_rev = is.reverseStack();
+	int bottom = is_rev.pop();
+	System.out.println(bottom);
+
+	//sort
+       IntStack is_sort = is.sort();  
+	is_sort.print(); 
+
+    }
 }
